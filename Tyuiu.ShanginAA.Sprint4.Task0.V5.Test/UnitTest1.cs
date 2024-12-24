900ae261b9ad1e3f8ae701b52453a7758db45e25
Tyuiu.ShanginAA@outlook.com <Tyuiu.ShanginAA@outlook.com>
2024-12-25 04:00:11 +0500

    -

---
+++ Tyuiu.ShanginAA.Sprint4.Task0.V5.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.ShanginAA.Sprint4.Task0.V5.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
