b9dfeba9de279d211b8bd9cac09f62ef300c117e
Tyuiu.ShanginAA@outlook.com <Tyuiu.ShanginAA@outlook.com>
2024-12-25 05:02:51 +0500

    -

--- Tyuiu.ShanginAA.Sprint4.Task1.V18.Lib/DataService.cs
+++ Tyuiu.ShanginAA.Sprint4.Task1.V18.Lib/DataService.cs
@@ -6,7 +6,15 @@ namespace Tyuiu.ShanginAA.Sprint4.Task1.V18.Lib
     {
         public int Calculate(int[] array)
         {
-            
+            int sum = 0;
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    sum += array[i];
+                }
+            }
+            return sum;
         }
     }
 }
--- Tyuiu.ShanginAA.Sprint4.Task1.V18.Test/DataServiceTest.cs
+++ Tyuiu.ShanginAA.Sprint4.Task1.V18.Test/DataServiceTest.cs
@@ -8,6 +8,14 @@ namespace Tyuiu.ShanginAA.Sprint4.Task1.V18.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+
+            int[] array = { 9, 8, 7, 6, 5 };
+
+            int wait = 21;
+            int res = ds.Calculate(array);
+
+            Assert.AreEqual(wait, res);
         }
     }
 }
